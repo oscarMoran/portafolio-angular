e6ed06226ee51f45513c4e85345c2ba59c3c6378
Oscar Moran <lucmorang@outlook.com>
2019-03-28 14:15:28 -0600

    Template sin rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/Shared/header/header.component';
+import { FooterComponent } from './Components/Shared/footer/footer.component';
+import { PortafolioComponent } from './Components/Pages/portafolio/portafolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    PortafolioComponent
   ],
   imports: [
     BrowserModule
